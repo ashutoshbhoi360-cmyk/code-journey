62faa4298b079cd5cafb89e0fd0af43cb9c65c89
ashutoshbhoi360-cmyk <ashutoshbhoi360@gmail.com>
2026-09-19 16:00:56 +0530

    first C program: right triangle area calculator

---
+++ area_calc.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+int main() {
+    float h, b, a;
+    printf("enter the hight and base");
+    scanf("%f, %f", &h, &b);
+    a = (h * b) / 2;
+printf("area = %g", a);
+    return 0;
+}
