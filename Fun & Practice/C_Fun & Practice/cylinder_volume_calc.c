f7710b9fe32164498a513a49f54119e21c4b3e00
ashutoshbhoi360-cmyk <ashutoshbhoi360@gmail.com>
2026-09-20 20:32:55 +0530

    fix: scanf reads radius first, matching the prompt (was &h, &r)

--- Fun & Practice/C_Fun & Practice/cylinder_volume_calc.c
+++ Fun & Practice/C_Fun & Practice/cylinder_volume_calc.c
@@ -2,7 +2,7 @@
 int main() {
     float h, r, volume;
     printf("enter the radius & height of cylinder\n");
-    scanf("%f,%f", &h, &r);
+    scanf("%f,%f", &r, &h);
     volume = 3.14159*r*r*h;
     printf("the volume of cylinder is %g\n", volume);
     return 0;
