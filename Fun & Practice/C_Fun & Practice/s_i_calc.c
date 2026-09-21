84e20ff943f7ddb559809027bfc4af874f088be9
ashutoshbhoi360-cmyk <ashutoshbhoi360@gmail.com>
2026-09-21 13:42:23 +0530

    add: simple interest calculator (P*R*T/100) with total amount + user instructions

---
+++ Fun & Practice/C_Fun & Practice/s_i_calc.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main() {
+    float p, r, t, i;
+    printf("please enter the principal amount, interest in percentage & time in years.\n");
+    printf("make sure the order of entry is  principal, interest rate & time.\n");
+    printf("note: separate principal, interest rate & time with commas, otherwise the program will crash.\n");
+    scanf("%f, %f, %f", &p, &r, &t);
+    i = (p*r*t)/100.0;
+    printf("simple interest is %g & total amount to gain or pay: %g.\n", i, p+i);
+    return 0;
+}
