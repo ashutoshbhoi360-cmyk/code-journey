a56d01ce4dee63d61291b58a122fa1f73811d757
ashutoshbhoi360-cmyk <ashutoshbhoi360@gmail.com>
2026-09-22 17:10:00 +0530

    add: divisibility checker upgraded with if-else — real yes/no answers + divisor-0 guard (nested conditions)

---
+++ C_Fun & Practice/08_dd_dr_ifelse.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+int main() {
+    int dd, dr;
+    // remember: dd means dividend & dr means divisor here
+    printf("enter the number that you want to check whether the number is divisible by second number and enter the second number also separating using comma.\n");
+    printf("please make sure the order of entry is dividend, divisor\n");
+    scanf("%d, %d", &dd, &dr);
+    if (dr != 0) {
+        if (dd%dr == 0) {
+            printf("yes, %d is divisible by %d", dd, dr);
+        }
+        else {
+            printf("no, %d is not divisible by %d", dd, dr);
+        }
+    }
+    else {
+        printf("error, divisor can't be 0");
+    }
+    return 0;
+}
