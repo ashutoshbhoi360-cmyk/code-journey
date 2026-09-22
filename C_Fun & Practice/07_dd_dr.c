406114a037ea5f5aa3021e73976d73103439d1a4
ashutoshbhoi360-cmyk <ashutoshbhoi360@gmail.com>
2026-09-22 06:28:36 +0530

    add: divisibility checker (dd % dr == 0, boolean result) — general, not just 97

---
+++ C_Fun & Practice/07_dd_dr.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main() {
+    int dd, dr;
+    // remember: dd means dividend & dr means divisor here
+    printf("enter the number that you want to check wheather the number is divisable by second number and enter the second number also separating using comma.\n");
+    printf("if output of program is 0 thats mean the first number isn't divisable by second number\n");
+
+    printf("please make sure the order of entry is dividend, divisor\n");
+    scanf("%d, %d", &dd, &dr);
+    printf("%d", dd%dr == 0);
+    return 0;
+}
