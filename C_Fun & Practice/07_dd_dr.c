96f85fe0bb621cdab1ed3dfd0e019b323f9c4dd1
ashutoshbhoi360-cmyk <ashutoshbhoi360@gmail.com>
2026-09-22 06:33:23 +0530

    fix: spelling in user prompts (whether, divisible, that means)

--- C_Fun & Practice/07_dd_dr.c
+++ C_Fun & Practice/07_dd_dr.c
@@ -2,8 +2,8 @@
 int main() {
     int dd, dr;
     // remember: dd means dividend & dr means divisor here
-    printf("enter the number that you want to check wheather the number is divisable by second number and enter the second number also separating using comma.\n");
-    printf("if output of program is 0 thats mean the first number isn't divisable by second number\n");
+    printf("enter the number that you want to check whether the number is divisible by second number and enter the second number also separating using comma.\n");
+    printf("if output of program is 0 that means the first number isn't divisible by second number\n");
 
     printf("please make sure the order of entry is dividend, divisor\n");
     scanf("%d, %d", &dd, &dr);
